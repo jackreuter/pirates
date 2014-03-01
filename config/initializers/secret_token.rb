6c2447e9da3c09ca434b60e3b3fd4f70f84a08c2
jreuter <jreuter@wesleyan.edu>
2014-03-01 11:30:42 -0500

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pirates::Application.config.secret_key_base = 'e6ad83bab61ad337781ba57a4b3c23395f1d5881d18f38824125f6f3866961ca8d82794b2f69c9850c603e9409dd5d75da9c3a2be2219078d5d2ff3fba726062'
